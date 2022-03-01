f2153c19d63ad0699bda5b7b3e73375ab037c12d
dairappa <53238927+dairappa@users.noreply.github.com>
2022-03-01 01:58:59 +0000

    とりあえず動作するように変更

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+// https://zenn.dev/alliana_ab2m/articles/how-to-write-discord-bot-in-typescript のサンプルコードをほぼコピペ
+// TODO: オリジナルのコードが増えたらこの注釈を消す
+
+import {Message, Client} from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+  intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES'],
+});
+
+client.once('ready', () => {
+  console.log('Ready!');
+  console.log(client.user?.tag ?? 'undefined user');
+});
+
+client.on('messageCreate', async (message: Message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith('!ping')) {
+    message.channel.send('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
