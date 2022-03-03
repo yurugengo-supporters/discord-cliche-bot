526c4f7c7651a58971987a49f11db31cfdb5bc2c
dairappa <53238927+dairappa@users.noreply.github.com>
2022-03-03 13:11:55 +0000

    GithubのID取得とInviteまで ただしInviteが権限不足（？）で失敗する

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@
 // TODO: オリジナルのコードが増えたらこの注釈を消す
 
 import {Message, Client} from 'discord.js';
+import {Octokit} from 'octokit';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const octokit = new Octokit({auth: process.env.GITHUB_TOKEN});
+
 
 const client = new Client({
   intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES'],
@@ -25,6 +30,26 @@ client.on('messageCreate', async (message: Message) => {
     message.channel.send(`Inviting ${userName}...`);
 
     // TODO: Githubへの招待処理
+    const invitedUserResponse = await octokit.request(
+        'GET /users/{username}', {
+          username: userName,
+        },
+    );
+
+    if (invitedUserResponse.status != 200) {
+      console.log(`${userName} is not valid user`);
+      return;
+    }
+
+    console.log(JSON.stringify(invitedUserResponse.data));
+
+    const invitationResponse = await octokit.request(
+        'POST /orgs/{org}/invitations', {
+          org: 'yurugengo-supporters',
+          invitee_id: invitedUserResponse.data.id,
+        });
+
+    console.log(JSON.stringify(invitationResponse.data));
   }
 });
 
