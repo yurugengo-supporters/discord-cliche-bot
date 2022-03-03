12e0e7dcd3aafb6c4984596227b2a4a6136f5c64
dairappa <53238927+dairappa@users.noreply.github.com>
2022-03-03 22:34:01 +0000

    エラー処理などを追加して、一旦メインのフローは動くようにした

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-// https://zenn.dev/alliana_ab2m/articles/how-to-write-discord-bot-in-typescript のサンプルコードをほぼコピペ
-// TODO: オリジナルのコードが増えたらこの注釈を消す
-
 import {Message, Client} from 'discord.js';
 import {Octokit} from 'octokit';
 
@@ -10,10 +7,44 @@ dotenv.config();
 
 const octokit = new Octokit({auth: process.env.GITHUB_PAT});
 
-
 const client = new Client({
   intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES'],
 });
+
+const fetchUserData = async (userName: string) => {
+  try {
+    const invitedUserResponse = await octokit.request(
+        'GET /users/{username}', {
+          username: userName,
+        },
+    );
+
+    if (invitedUserResponse.status != 200) {
+      console.log(`${userName} is not valid user`);
+      return null;
+    }
+
+    return invitedUserResponse.data;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+const inviteUser = async (userId: number) => {
+  try {
+    const invitationResponse = await octokit.request(
+        'POST /orgs/{org}/invitations', {
+          org: 'yurugengo-supporters',
+          invitee_id: userId,
+        });
+
+    return invitationResponse;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
 
 const githubWakeKeyword = '!ghinvite';
 
@@ -27,29 +58,21 @@ client.on('messageCreate', async (message: Message) => {
   if (message.content.startsWith(githubWakeKeyword)) {
     const userName = message.content.slice(githubWakeKeyword.length).trim();
 
-    message.reply(`Inviting ${userName}...`);
-
-    // TODO: Githubへの招待処理
-    const invitedUserResponse = await octokit.request(
-        'GET /users/{username}', {
-          username: userName,
-        },
-    );
-
-    if (invitedUserResponse.status != 200) {
-      console.log(`${userName} is not valid user`);
+    const invitedUser = await fetchUserData(userName);
+    if (!invitedUser) {
+      console.log('invalid user');
+      message.reply(`${userName}は存在しないGithubアカウントです。`);
       return;
     }
 
-    console.log(JSON.stringify(invitedUserResponse.data));
+    const invitationResponse = await inviteUser(invitedUser?.id);
 
-    const invitationResponse = await octokit.request(
-        'POST /orgs/{org}/invitations', {
-          org: 'yurugengo-supporters',
-          invitee_id: invitedUserResponse.data.id,
-        });
+    if (!invitationResponse) {
+      message.reply(`${userName}の招待に失敗しました。すでにメンバーになっている可能性があります。`);
+      return;
+    }
 
-    console.log(JSON.stringify(invitationResponse.data));
+    message.reply(`${userName}を組織に招待しました。`);
   }
 });
 
