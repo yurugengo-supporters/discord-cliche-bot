cff5b67dd0576ca015907f86889aa607df884503
dairappa <53238927+dairappa@users.noreply.github.com>
2022-03-05 14:17:47 +0000

    一応Asyncにしておく とは言え、このPromiseのママのほうが読みやすいのではという気はとてもする

--- src/command_register.ts
+++ src/command_register.ts
@@ -20,7 +20,17 @@ const command = new SlashCommandBuilder()
 const rest = new REST({version: '9'})
     .setToken(`${process.env.DISCORD_TOKEN}`);
 
-rest.post(Routes.applicationCommands(botClientId), {body: command})
-    .then(() => console.log('Successfully registered application commands.'))
-    .catch(console.error);
+const register = async () => {
+  try {
+    const response = await rest.post(
+        Routes.applicationCommands(botClientId), {body: command});
+    console.log('Successfully registered application commands.');
+    console.log(JSON.stringify(response));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+register();
+
 
