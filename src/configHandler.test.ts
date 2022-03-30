66e66aafb5f6e30ce3cf4ab30423b46dd1d8d222
dairappa <53238927+dairappa@users.noreply.github.com>
2022-03-30 13:21:21 +0000

    テストコードの言い訳を追加

--- src/configHandler.test.ts
+++ src/configHandler.test.ts
@@ -1,13 +1,16 @@
 import {clicheBotConfig} from './configHandler';
 
-test.skip('has github pat', () => {
-  expect(clicheBotConfig.githubPat.length).not.toBe(0);
-});
+// 環境変数として各値が正常にセットされているかのテスト。外部環境に依存するためあんまりきれいなテストではないが、設定ミスを防ぐために防衛的に入れておく
+describe('environment val test', () => {
+  test('has github pat', () => {
+    expect(clicheBotConfig.githubPat.length).not.toBe(0);
+  });
 
-test.skip('has bot client id', () => {
-  expect(clicheBotConfig.botClientId.length).not.toBe(0);
-});
+  test('has bot client id', () => {
+    expect(clicheBotConfig.botClientId.length).not.toBe(0);
+  });
 
-test.skip('has discord token', () => {
-  expect(clicheBotConfig.discordToken.length).not.toBe(0);
+  test('has discord token', () => {
+    expect(clicheBotConfig.discordToken.length).not.toBe(0);
+  });
 });
