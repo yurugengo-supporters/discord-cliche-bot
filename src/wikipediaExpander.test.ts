cddf590b0dfbac14a98bc2e260322ed0c5e5420e
dairappa <53238927+dairappa@users.noreply.github.com>
2022-04-26 14:19:55 +0000

    Wikipediaの中身を取得するテストを追記

--- src/wikipediaExpander.test.ts
+++ src/wikipediaExpander.test.ts
@@ -1,4 +1,4 @@
-import {expandWikipediaUrl} from './wikipediaExpander';
+import {expandWikipediaUrl, expandWikipediaUrlToData} from './wikipediaExpander';
 
 // eslint-disable-next-line max-len
 const wikipediaSiteSample = 'https://ja.wikipedia.org/wiki/%E3%82%AA%E3%82%B0%E3%83%AA%E3%82%AD%E3%83%A3%E3%83%83%E3%83%97';
@@ -15,5 +15,9 @@ describe('simple url', () => {
 
   test('not wikipedia URL', () => {
     expect(expandWikipediaUrl(`オグリキャップ ${netkeibaUrl} ${wikipediaSiteSample}`)).toBe(wikipediaSiteSample);
+  });
+
+  test('expand wikipedia data', () => {
+    expect(expandWikipediaUrlToData('オグリキャップ')?.title).toBe('オグリキャップ');
   });
 });
--- src/wikipediaExpander.ts
+++ src/wikipediaExpander.ts
@@ -7,3 +7,12 @@ export const expandWikipediaUrl: (text: string) => string | undefined = (text) =
     }
   }
 };
+
+export const expandWikipediaUrlToData: (text: string) =>
+{title: string, summary: string, thumbnailUrl:string} | undefined = (text) => {
+  for (const url of getUrls(text)) {
+    if (url.startsWith('https://ja.wikipedia.org/wiki/')) {
+      return {title: text, summary: text, thumbnailUrl: text};
+    }
+  }
+};
