9a9facf141ee630579174befb4d6c4f7c8dac6bd
dairappa <53238927+dairappa@users.noreply.github.com>
2022-04-27 21:11:54 +0000

    短縮URLにも対応

--- src/wikipediaExpander.test.ts
+++ src/wikipediaExpander.test.ts
@@ -4,32 +4,37 @@ import {existsWikipediaUrl, expandWikipediaUrl, expandWikipediaUrlToData} from '
 const wikipediaSiteSample = 'https://ja.wikipedia.org/wiki/%E3%82%AA%E3%82%B0%E3%83%AA%E3%82%AD%E3%83%A3%E3%83%83%E3%83%97';
 // eslint-disable-next-line max-len
 const wikipediaMobileSiteSample = 'https://ja.m.wikipedia.org/wiki/%E3%82%AA%E3%82%B0%E3%83%AA%E3%82%AD%E3%83%A3%E3%83%83%E3%83%97';
+const wikipediaRefirectedSample = 'https://w.wiki/3CnA';
 const netkeibaUrl = 'https://db.netkeiba.com/horse/1985102167/';
 
 describe('Wikipedia Expander', () => {
-  test('simple url', () => {
-    expect(expandWikipediaUrl(wikipediaSiteSample)).toBe(wikipediaSiteSample);
+  test('simple url', async () => {
+    expect(await (expandWikipediaUrl(wikipediaSiteSample))).toBe(wikipediaSiteSample);
   });
 
-  test('mobile url', () => {
-    expect(expandWikipediaUrl(wikipediaMobileSiteSample)).toBe(wikipediaSiteSample);
+  test('redirected url', async () => {
+    expect(await (expandWikipediaUrl(wikipediaRefirectedSample))).toBe(wikipediaSiteSample);
   });
 
-  test('url in text', () => {
-    expect(expandWikipediaUrl(`オグリキャップ ${wikipediaSiteSample}`)).toBe(wikipediaSiteSample);
+  test('mobile url', async () => {
+    expect(await (expandWikipediaUrl(wikipediaMobileSiteSample))).toBe(wikipediaSiteSample);
   });
 
-  test('wikipedia URL and other URL', () => {
-    expect(expandWikipediaUrl(`オグリキャップ ${netkeibaUrl} ${wikipediaSiteSample}`)).toBe(wikipediaSiteSample);
+  test('url in text', async () => {
+    expect(await (expandWikipediaUrl(`オグリキャップ ${wikipediaSiteSample}`))).toBe(wikipediaSiteSample);
   });
 
-  test('not wikipedia URL', () => {
-    expect(existsWikipediaUrl(`オグリキャップ ${netkeibaUrl}`)).toBe(false);
+  test('wikipedia URL and other URL', async () => {
+    expect(await (expandWikipediaUrl(`オグリキャップ ${netkeibaUrl} ${wikipediaSiteSample}`))).toBe(wikipediaSiteSample);
+  });
+
+  test('not wikipedia URL', async () => {
+    expect(await (existsWikipediaUrl(`オグリキャップ ${netkeibaUrl}`))).toBe(false);
   });
 
 
-  test('wikipedia URL', () => {
-    expect(existsWikipediaUrl(`オグリキャップ ${wikipediaSiteSample}`)).toBe(true);
+  test('wikipedia URL', async () => {
+    expect(await (existsWikipediaUrl(`オグリキャップ ${wikipediaSiteSample}`))).toBe(true);
   });
 
   test('expand wikipedia data', async () => {
--- src/wikipediaExpander.ts
+++ src/wikipediaExpander.ts
@@ -1,25 +1,33 @@
 import getUrls from 'get-urls';
 import wikipedia from 'wikipedia';
+import fetch from 'node-fetch';
+
 
 const wikipediaUrlPrefix = 'https://ja.wikipedia.org/wiki/';
 
 wikipedia.setLang('ja');
 
-export const existsWikipediaUrl: (text: string) => boolean = (text: string) => {
-  return !!expandWikipediaUrl(text);
+export const existsWikipediaUrl: (text: string) => Promise<boolean> = async (text: string) => {
+  return !!(await expandWikipediaUrl(text));
 };
 
-export const expandWikipediaUrl: (text: string) => string | undefined = (text) => {
+export const expandWikipediaUrl: (text: string) => Promise<string | undefined> = async (text) => {
   for (const url of getUrls(text)) {
     const urlNormalized = url.replace('ja.m.wikipedia', 'ja.wikipedia');
-    if (urlNormalized.startsWith(wikipediaUrlPrefix)) {
-      return urlNormalized;
+
+    const response = await fetch(urlNormalized, {method: 'GET'});
+    const finalUrl = response.url;
+
+    console.log(finalUrl);
+
+    if (finalUrl.startsWith(wikipediaUrlPrefix)) {
+      return finalUrl;
     }
   }
 };
 
 export const expandWikipediaUrlToData = async (text: string) => {
-  const wikipediaUrl = expandWikipediaUrl(text);
+  const wikipediaUrl = await expandWikipediaUrl(text);
 
   if (!wikipediaUrl) {
     return undefined;
